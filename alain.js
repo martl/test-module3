d0ac7defde16de7287c5e471b0cb60b985d4353d
Alain Rohrbacher <45689844+alainrohrbacher@users.noreply.github.com>
2019-06-26 12:03:26 +0200

    Add files via upload

---
+++ alain.js
@@ -0,0 +1,26 @@
+// 3.
+
+let stringEins = "lastName,";
+let stringZwei = "firstName";
+let concatedString = stringEins.concat(stringZwei);
+
+console.log(concatedString);
+
+//6.
+var arrayUngerade = [];
+for(var i=1; i<20; i=i+2) {
+    arrayUngerade.push(i);
+    console.log(`ungerade zahl ${JSON.stringify(arrayUngerade)}`);
+}
+var arrayGerade = [];
+for(var i=2; i<20; i=i+2) {
+    arrayGerade.push(i);
+    console.log(`gerade zahl ${JSON.stringify(arrayGerade)}`);
+}
+//8.
+var stringWert = "maria, jane, jones";
+var grossBuchstaben = stringWert.toUpperCase();
+console.log(grossBuchstaben);
+
+//5.
+
